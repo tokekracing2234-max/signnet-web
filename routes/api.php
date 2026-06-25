cbfed19da1c37b67dfa2cfd5e685ec2e73bf7a96
YteamSyndicate <kembarangetret@gmail.com>
2026-06-26 06:58:48 +0700

    fix train model lagi

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use App\Http\Controllers\Api\ModelSyncController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SignController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -10,3 +11,4 @@ Route::get('/user', function (Request $request) {
 
 Route::post('/sync-model', [ModelSyncController::class, 'receiveModel']);
 Route::post('/receive-model', [ModelSyncController::class, 'receiveModel']);
+Route::post('/update-model-files', [SignController::class, 'updateModelFiles']);
